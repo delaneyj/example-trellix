16ae94b20750ad3837469e0a457b2ca3679b8e6a
Delaney Gillilan <delaneygillilan@gmail.com>
2025-07-12 18:38:00 -0700

    add compose

--- start.sh
+++ start.sh
@@ -1,9 +1,3 @@
-fallocate -l 512M /swapfile
-chmod 0600 /swapfile
-mkswap /swapfile
-echo 10 > /proc/sys/vm/swappiness
-swapon /swapfile
-echo 1 > /proc/sys/vm/overcommit_memory
-
+#!/bin/sh
 npx prisma migrate deploy
 npm run start
